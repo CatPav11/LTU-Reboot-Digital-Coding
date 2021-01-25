d53ca813997668b996feb53c51922b1e2d9c91c0
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-25 11:08:35 +0000

    Update task-3.1.4.js
    Add variables and arithmetic operators

---
+++ module-3/lesson-1/scripts/task-3.1.4.js
@@ -0,0 +1,12 @@
+var sunnyDays = 5
+var rainyDays = 7
+
+console.log(sunnyDays*rainyDays);
+
+alert(rainyDays-sunnyDays);
+
+document.write(sunnyDays+rainyDays);
+
+console.log(sunnyDays/rainyDays);
+
+document.write(sunnyDays%rainyDays);
