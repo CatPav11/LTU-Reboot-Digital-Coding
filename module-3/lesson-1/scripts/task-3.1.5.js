b1408eacbff1159be9f471e43521cfbf5e4d3b87
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-25 12:03:17 +0000

    Update task-3.1.5.js
    Add variables

--- module-3/lesson-1/scripts/task-3.1.5.js
+++ module-3/lesson-1/scripts/task-3.1.5.js
@@ -3,11 +3,8 @@
 var firstName = 'Catherine ';
 var lastName = 'Pavitt'; 
 
-
 console.log(firstName + lastName);
-
 document.write(firstName + lastName);
-
 alert(firstName + lastName);
 
 let fullName = firstName + lastName
