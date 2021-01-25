036adbd82c62d7af550a57316db07cacdbe01192
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-25 12:03:02 +0000

    Update task-3.1.6.js
    Add two variables for bill and tip. Concocentate two variables to make a total.

---
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -0,0 +1,8 @@
+var billPreTip = 35
+var tipTotal = billPreTip / 10
+
+var totalBill = billPreTip + tipTotal
+
+console.log("Your total bill, with tip is £" + totalBill + ". ");
+document.write("Your total bill, with tip is £" + totalBill + ". ");
+document.write("This includes a tip of " + tipTotal);
