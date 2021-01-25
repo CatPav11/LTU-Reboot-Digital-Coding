ae2fbd179c96f662968165ed2feccd96547d1547
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-25 11:29:38 +0000

    Update task-3.1.5.js
    Add two variables: first name and last name and concatenate them together

---
+++ module-3/lesson-1/scripts/task-3.1.5.js
@@ -0,0 +1,17 @@
+
+
+var firstName = 'Catherine ';
+var lastName = 'Pavitt'; 
+
+
+console.log(firstName + lastName);
+
+document.write(firstName + lastName);
+
+alert(firstName + lastName);
+
+let fullName = firstName + lastName
+
+console.log(fullName);
+document.write(fullName);
+alert(fullName);
