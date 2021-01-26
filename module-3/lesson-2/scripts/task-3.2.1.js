e1088eba3a038710e0a75a3ad7c9bbd494afd1ab
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-26 10:01:02 +0000

    Update task-3.2.1.js
    Add declare function and invoke function message

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,12 @@
+
+// Declare a function
+
+function dogFacts() {
+    console.log("There are many different breeds of dogs");
+    document.write("Dogs like to bark");
+    alert("Dogs wag their tails when they are happy");
+}
+
+//Invoke a function
+
+dogFacts();
