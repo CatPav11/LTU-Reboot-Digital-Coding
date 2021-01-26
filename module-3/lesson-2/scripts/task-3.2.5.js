c6a99744f6a4e156235bdf56337c517c0476a290
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-26 11:33:17 +0000

    Update task-3.2.5.js
    Add temperature variable and if else statements

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,15 @@
+var temperature = -1;
+
+if (temperature < 0) {
+    document.write("Please stay inside");
+} else if (temperature < 30) {
+    document.write("Please wear a coat and a hat");
+} else if (temperature < 50) {
+    document.write("Please wear a coat");
+} else {
+    document.write("Just pants and a vest is fine");
+}
+
+
+
+
