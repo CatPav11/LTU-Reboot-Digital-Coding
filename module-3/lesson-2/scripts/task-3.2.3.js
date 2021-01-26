ebb105bbe8469ba50c3fae8ae8dc854b8d3c0b9b
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-26 10:50:14 +0000

    Update task-3.2.3.js
    add full name function and return values to use later in my programme

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -11,4 +11,20 @@ var message = greet('Hello', 'Luke');
 
 // ADD YOUR CODE BELOW HERE
 
+
+function showFullName(fName, sName){
+  var fullName = fName + ' ' + sName + '<br>';
+  return fullName;
+}
+
+var person1 = showFullName('Catherine', 'Pavitt');
+  document.write(person1);
+
+
+var person2 = showFullName('John', 'Smith');
+  document.write(person2);
+
+var person3 = showFullName('Sue', 'Jones');
+document.write(person3);
+
 // ADD YOUR CODE ABOVE HERE
