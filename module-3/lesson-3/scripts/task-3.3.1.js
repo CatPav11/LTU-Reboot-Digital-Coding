22aeac412fc41630a235e04ec86a9b333badd29b
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-27 10:10:55 +0000

    Update task-3.3.1.js
    create percentage calculator

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,8 @@
+function percentageCalculator (number, percentage) {
+    var result = (number / 100) * percentage;
+    return result;
+}
+
+console.log(percentageCalculator(400, 8));
+console.log(percentageCalculator(200, 2.5));
+console.log(percentageCalculator(250, 22));
