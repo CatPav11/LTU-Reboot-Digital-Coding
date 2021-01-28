f7bf5c460b31ed1cbb653b9090fbe9d5f4029fc0
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-01-28 10:50:23 +0000

    Create task-3.4.3.js
    add favourite food array and loop

---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,7 @@
+
+var favouriteFoods = [ 'Steak', 'Eggs', 'Prawns', 'Roast beef', 'Crisps', 'Grapes'];
+
+
+for (var i = 1; i = favouriteFoods.length; i++); {
+    console.log(favouriteFoods[i]);
+}
