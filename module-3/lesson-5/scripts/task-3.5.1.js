127e3973e86a5c87db4e61e1f486db011a255970
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-02-01 10:34:51 +0000

    Update task-3.5.1.js
    add return function

--- module-3/lesson-5/scripts/task-3.5.1.js
+++ module-3/lesson-5/scripts/task-3.5.1.js
@@ -1,12 +1,8 @@
 function shoppingCartTotalPrice(arr){
     let totalPrice = 0;
 
-    for ( let index = 0; index < arr.length; index++ ) {
-       
-        // console.log(arr[ index ].price );
-
+    for ( let index = 0; index < arr.length; index++ ) {      
         totalPrice = totalPrice + arr[index].price * arr[index].quantity;
-
     }
 
     return totalPrice.toFixed(2);
