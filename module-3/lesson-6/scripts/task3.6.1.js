4216decb07bba241d4f7d4d52f2718e8b68fcde2
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-02-02 10:53:43 +0000

    Create task3.6.1.js
    select image and change src attribute, select second paragraph and add styles

---
+++ module-3/lesson-6/scripts/task3.6.1.js
@@ -0,0 +1,7 @@
+let logo = document.querySelector('img');
+    logo.setAttribute('src', 'https://logomaster.ai/static/media/sample.c8bf2b04.svg');
+
+let secondParagraph = document.querySelectorAll('p')[1];
+    secondParagraph.style.border= '4px solid goldenrod';
+    secondParagraph.style.marginTop = '58px';
+    secondParagraph.style.padding = '50px';
