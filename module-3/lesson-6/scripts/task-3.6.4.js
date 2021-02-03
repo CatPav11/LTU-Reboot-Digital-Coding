941910aa14c9b077dd5e533abc6f514f5b10346a
CatPav11 <77329561+CatPav11@users.noreply.github.com>
2021-02-03 10:00:12 +0000

    Create task-3.6.4.js
    Add event listener to anchor tag we created in HTML doc

---
+++ module-3/lesson-6/scripts/task-3.6.4.js
@@ -0,0 +1,9 @@
+
+let disabledLink = document.querySelector('a.disabled');
+
+disabledLink.addEventListener('click', function( event ){
+
+    event.preventDefault();
+    console.log('clicked');
+
+});
